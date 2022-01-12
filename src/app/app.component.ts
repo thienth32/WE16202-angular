87d2b83b2a577317e4625f45cc9ec67ed66341f0
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-01-12 12:22:29 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Inventor} from '../models/Inventor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WE16202 - angular';
   count = 0;
 
-  inventors: Inventor[] = [
-    {
-      id: 1, first: "thienth", last: "tran", year: 1991, passed: 2090
-    },
-    {
-      id: 2, first: "Albert", last: "Einstein", year: 1879, passed: 1955
-    }
-  ];
+  inventors: Inventor[] = [];
+
+  ngOnInit(): void {
+    let that = this;
+    setTimeout(() => {
+      that.inventors = [
+        {
+          id: 1, first: "thienth", last: "tran", year: 1991, passed: 2090
+        },
+        {
+          id: 2, first: "Albert", last: "Einstein", year: 1879, passed: 1955
+        }
+      ];
+    }, 3000)
+  }
 
   increaseCount(){
     this.count++;
