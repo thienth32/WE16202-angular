cd90ae4b61c301dce5ea930e00caee3c140b8942
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-02-11 08:38:19 +0700

    reactive form

--- src/app/screens/project-add/project-add.component.ts
+++ src/app/screens/project-add/project-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-project-add',
@@ -8,13 +8,19 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ProjectAddComponent implements OnInit {
   projectForm = new FormGroup({
-    name: new FormControl(),
+    name: new FormControl(null, Validators.required),
     date_start: new FormControl(),
-    team_size: new FormControl()
+    team_size: new FormControl(null, [
+      Validators.required, 
+      Validators.min(0)
+    ])
   });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submitForm(){
+    console.log(this.projectForm.value);
+  }
 }
